3d9f40cdaf5ef8b25569037fc73cfe0cca3f97a3
tcnksm <nsd22843@gmail.com>
2016-10-24 12:50:52 +0900

    Check existing release before creating

--- cmd/sync-github-release/main.go
+++ cmd/sync-github-release/main.go
@@ -1,6 +1,7 @@
 /*
 Command 'sync-github-release' syncs github release pages with one with
-the other. For example, this is used to sync fork and upstream release.
+the other. For example, this is used to sync fork and upstream release
+(Currently, it doesn't sync artifacts).
 
   $ sync-github-release REPO DIST_OWNER SRC_OWNER
 
@@ -8,8 +9,6 @@ For example, if you want to sync releases on https://github.com/tcnksm/xxxx with
 https://github.com/deeeet/xxxx
 
   $ sync-github-release xxxx deeeet tcnksm
-
-It doensn't sync uploaded artifacts.
 
 To use this command, you need to prepare GitHub API Token (with repo priviledge).
 You can set it via TOKEN env var.
@@ -51,17 +50,26 @@ func main() {
 	client := github.NewClient(tc)
 
 	// List all releases on upstream repository
-	releases, _, err := client.Repositories.ListReleases(srcOwner, repo, nil)
+	srcReleases, _, err := client.Repositories.ListReleases(srcOwner, repo, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	distReleases, _, err := client.Repositories.ListReleases(distOwner, repo, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Create release which is on upsteram on fork repository
-	log.Printf("[INFO] Found %d releases", len(releases))
+	log.Printf("[INFO] Found %d releases on src", len(srcReleases))
 	var success int
-	for _, release := range releases {
-		log.Println("Sync", *release.TagName)
+	for _, release := range srcReleases {
+		if contains(distReleases, release) {
+			log.Printf("[INFO] %s is already synced", *release.TagName)
+			continue
+		}
 
+		log.Println("Sync", *release.TagName)
 		_, _, err = client.Repositories.CreateRelease(distOwner, repo, &github.RepositoryRelease{
 			Name:       release.TagName,
 			TagName:    release.TagName,
@@ -71,8 +79,7 @@ func main() {
 		})
 
 		if err != nil {
-			log.Println("[ERROR] Failed to create release:", err)
-			continue
+			log.Fatal("[ERROR] Failed to create release:", err)
 		}
 
 		// Prevent to DDos to GitHub
@@ -81,4 +88,15 @@ func main() {
 	}
 
 	log.Printf("[INFO] Successfully sync %d releases", success)
+}
+
+// contains checks the given releases contains the given release.
+// It uses TagName for that check.
+func contains(releases []*github.RepositoryRelease, release *github.RepositoryRelease) bool {
+	for _, r := range releases {
+		if *r.TagName == *release.TagName {
+			return true
+		}
+	}
+	return false
 }
